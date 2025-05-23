32a019c461ba247f902092369c07c0a6c4feef21
roccohimel <rockyrbn1847@gmail.com>
2025-05-23 10:38:07 -0400

    Add files via upload

---
+++ txsearch-inst.py
@@ -0,0 +1,22 @@
+import subprocess
+import time
+subprocess.run("clear", shell=True)
+print("Installing TxSearch browsers")
+time.sleep(1)
+subprocess.run("clear", shell=True)
+print("Installing TxSearch browsers.")
+time.sleep(1)
+subprocess.run("clear", shell=True)
+print("Installing TxSearch browsers..")
+time.sleep(1)
+subprocess.run("clear", shell=True)
+print("Installing TxSearch browsers...")
+time.sleep(1)
+subprocess.run("clear", shell=True)
+subprocess.run("sudo apt-get install brave-browser", shell=True )
+print("Done!")
+subprocess.run("sudo apt-get install firefox-esr", shell=True)
+subprocess.run("git clone https://github.com/roccohimel/txsearch.git", shell=True)
+print("You can close this now. (^C)")
+
+
